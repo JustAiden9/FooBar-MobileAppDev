8d829f81d3221b42ba8855337c12bc8be3e493cd
Aiden <aidenbak9@gmail.com>
2025-08-26 09:32:37 -0500

    adeed ontapgesture string/loop and fixed the text to be a Text(display)

--- FooBar/ContentView.swift
+++ FooBar/ContentView.swift
@@ -9,15 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var counter = 0
+    @State private var display = "0"
+    @State private var color = Color.black
+    
     var body: some View {
-        VStack {
-            Text("\(counter)")
+        ZStack {
+            color.ignoresSafeArea()
+            Text(display)
                 .font(.system(size: 128))
                 .multilineTextAlignment(.center)
                 .preferredColorScheme(.dark)
-            
+                .onTapGesture {
+                    counter += 1
+                    if counter % 3 == 0 && counter % 5 == 0 {
+                        display = "Foo Bar"
+                        color = .red
+                    } else if counter % 3 == 0 {
+                        display = "Foo"
+                        color = .green
+                    } else if counter % 5 == 0 {
+                        display = "Bar"
+                        color = .yellow
+                    } else {
+                        display = String(counter)
+                        color = .black
+                    }
+                }
+                .onLongPressGesture {
+                    counter = 0
+                    display = "0"
+                    color = .black
+                }
         }
-        .padding()
     }
 }
 
